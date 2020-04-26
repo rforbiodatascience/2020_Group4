b741992d7a380be7910fc628208c4fd2aef4cb9e
shdam <soren.helweg@gmail.com>
2020-04-26 15:18:37 +0000

    Group toxins

--- R/02_clean.R
+++ R/02_clean.R
@@ -4,8 +4,46 @@ library(tidyverse)
 relative <- read_csv("data/_raw/relative.csv")
 
 # Clean data
-relative <- relative
-  # Do something
+SVMP <- relative %>% 
+  select(contains('SVMP (')) %>% 
+  rowSums()
+
+disintegrins <- relative %>% 
+  select(contains('disintegrin,')) %>% 
+  rowSums()
+
+lectins <- relative %>% 
+  select(contains('lectin')) %>% 
+  rowSums()
+
+neurotoxins <- relative %>% 
+  select(contains('NeuroToxin')) %>% 
+  rowSums()
+
+PLA2 <- relative %>% 
+  select(contains('PLA2')) %>% 
+  rowSums()
+
+
+relative <- relative %>% 
+  select(-contains('SVMP ('), 
+         -contains('disintegrin,'),
+         -contains('lectin'),
+         -contains('NeuroToxin'),
+         -contains('PLA2')
+         ) %>% 
+  mutate(
+    SVMP = SVMP,
+    disintegrin = disintegrins,
+    lectins = lectins,
+    neurotoxins = neurotoxins,
+    PLA2 = PLA2
+    )
+
+relative %>% 
+  select(-c("Snake", "Reference", "Note", "Sum, %")) %>% 
+  colSums() %>% 
+  View()
 
 
 # Write cleaned data
