fbfcbf6f0d02601f2823e072f23bca4bcfaa5a50
shdam <soren.helweg@gmail.com>
2020-04-27 08:15:07 +0000

    read sheet

--- R/01.2_load_new_data.R
+++ R/01.2_load_new_data.R
@@ -4,6 +4,15 @@
 
 library(googlesheets4)
 
-new_data <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=0')
-new_meta <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=1686090584')
+new_data <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=0',
+                       sheet = 1,
+                       col_types = 'cnnnn')
+new_meta <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=0',
+                       sheet = 2)
 
+
+
+new_data %>% 
+  replace_na(list(everything() = 0)) %>% 
+  View()
+new_data[is.na(new_data)] <- 0
