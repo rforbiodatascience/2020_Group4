43c26389d97e47a2a0edc48409c2fbe371066993
shdam <soren.helweg@gmail.com>
2020-05-06 07:55:13 +0000

    Code cleaning

--- R/01.2_load_new_data.R
+++ R/01.2_load_new_data.R
@@ -1,18 +1,17 @@
 # Add new data
-
 # install.packages("googlesheets4")
 
 library(googlesheets4)
 library(tidyverse)
 
-## Load data
+
+# Load data ---------------------------------------------------------------
 new_data <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=0',
                        sheet = 1,
                        col_types = 'cnnnn')
 new_meta <- read_sheet('https://docs.google.com/spreadsheets/d/1vLrvvQmQdvCtr6n0hjDbIoLiOv3cHVGx7MK_2w_WH3E/edit#gid=0',
                        sheet = 2)
-
-## Tidy data
+# Tidy data ---------------------------------------------------------------
 new_data <- new_data %>% 
   replace(is.na(.), 0) %>% 
   pivot_longer(-Toxin, names_to = "Snake", values_to = "value") %>% 
@@ -20,6 +19,8 @@ new_data <- new_data %>%
   left_join(new_meta, by = "Snake") %>% 
   mutate(`Unknown/Undetermined` = 100 - Reduce(`+`, select_if(., is.numeric)))
 
-## Write data
+
+
+# Write data --------------------------------------------------------------
 new_data %>% 
   write_csv('data/01.2_new_data.csv')
--- R/02_clean.R
+++ R/02_clean.R
@@ -9,131 +9,42 @@ data_raw <- read_csv("data/_raw/01_data_load_relative.csv")
 
 
 ###### Clean note column and add new column containing grouped regions
-#Condition in data set is that rownames ending with * indicates transcriptomic data
 Brazilian_cities <- c("Juazeiro", "Ceara", "Paraiba", "Pernambuco", "ilha de Itaparica", "Adult and young in Brazil")
 USA <- c("Colorado", "Arizona", "Idyllwild", "Loma Linda", "Phelan", "Catalina", "Texas", "Kentucky", "Missouri",
          "Florida", "Kansas", "Colorado", "Ohio", "New Mexico")
+
 data_clean <- data_raw %>% 
   rename(`SP (Serine Proteinase), %` = "SP (Serine roteinase), %",
          `α-NTx (α-NeuroToxin)` = "?-NTx (?-NeuroToxin), %") %>% 
   filter(!(str_to_lower(Note) %in% c("origin unknown", "pooled", "neonate", "adult")),
+         # Condition in data set is that rownames ending with * indicates transcriptomic data
          str_detect(Snake, '\\*', negate = TRUE)) %>% 
-  # filter_all(all_vars(!grepl("\\*",.))) %>% 
   mutate(new_col = case_when(#
                             detect_in_list(Note, USA) ~ "USA",
-                            # Note %in% USA ~ "USA",
-                            # str_detect(Note, "New Mexico") ~ "USA",
-                            # Note == "Texas" ~ "USA", 
-                            # Note == "Kentucky" ~ "USA",
-                            # Note == "Missouri" ~ "USA",
-                            # Note == "Florida" ~ "USA",
-                            str_detect(Note, "Caribbean") ~ "Caribbean",
-                            Note == "Carribean" ~ "Caribbean",
-                            # Note == "Caribbean neonate" ~ "Caribbean",
-                            # Note == "Caribbean adult" ~ "Caribbean",
-                            str_detect(Note, "Pacific") ~ "Pacific",
-                            # Note == "Pacific adult" ~ "Pacific",
-                            # Note == "Pacific neonate" ~ "Pacific",
-                            str_detect(str_to_lower(Note), "costa rica") ~ "Costa Rica",
-                            # Note == "Costa Rica (Pacific)" ~ "Costa Rica",
-                            # Note == "Costa Rica (Carribean)" ~ "Costa Rica",
-                            Note == "Columbia" ~ "Colombia",
-                            str_detect(Note, "Venezuelan") ~ "Venezuela",
-                            # Note == "Venezuelan juvenile" ~ "Venezuela",
-                            # Note == "Venezuelan adult" ~ "Venezuela",
                             Note %in% Brazilian_cities ~ "Brazil",
+                            str_detect(Note, "Caribbean") ~ "Caribbean",
+                            str_detect(Note, "Pacific") ~ "Pacific",
+                            str_detect(str_to_lower(Note), "costa rica") ~ "Costa Rica",
+                            str_detect(Note, "Venezuelan") ~ "Venezuela",
+                            str_detect(Note, "Mexican") ~ "Mexico",
+                            str_detect(Note, 'India') ~ "India",
+                            Note == "Carribean" ~ "Caribbean",
+                            Note == "Columbia" ~ "Colombia",
                             Note == "Marocco" ~ "Morocco",
                             Note == "Tunesia" ~ "Tunisia",
-                            # Note == "Juazeiro" ~ "Brazil",
-                            # Note == "Ceara" ~ "Brazil",
-                            # Note == "Paraiba" ~ "Brazil",
-                            # Note == "Pernambuco" ~ "Brazil",
-                            # Note == "ilha de Itaparica" ~ "Brazil",
-                            # Note == "Adult and young in Brazil" ~ "Brazil",
-                            # Note == "Catalina Island 1" ~ "USA",
-                            # Note == "Phelan 1" ~ "USA",
-                            # Note == "Idyllwild 1" ~ "USA",
-                            # Note == "Phelan 2" ~ "USA",
-                            # Note == "Loma Linda 1" ~ "USA",
-                            # Note == "Idyllwild 3" ~ "USA",
-                            # Note == "Idyllwild 2" ~ "USA",
-                            # Note == "Phelan 3" ~ "USA",
-                            # Note == "Loma Linda 2" ~ "USA",
-                            # Note == "Catalina Island 2" ~ "USA",
-                            # Note == "Catalina Island 3" ~ "USA",
-                            # Note == "Arizona (A-101)" ~ "USA",
-                            # Note == "Arizona (F-303)" ~ "USA",
-                            # Note == "Arizona (F-307)" ~ "USA",
-                            # Note == "Arizona (E-105)" ~ "USA",
-                            # Note == "Arizona (E-104)" ~ "USA",
-                            # Note == "Arizona (E-106)" ~ "USA",
-                            # Note == "Arizona (E-203)" ~ "USA",
-                            # Note == "New Mexico (F-301)" ~ "USA",
-                            # Note == "Arizona (A-103)" ~ "USA",
-                            # Note == "Arizona (E-202)" ~ "USA",
-                            # Note == "Arizona (E-204)" ~ "USA",
-                            # Note == "Arizona (A-108)" ~ "USA",
-                            # Note == "Arizona (B-110)" ~ "USA",
-                            # Note == "Arizona (C-107)" ~ "USA",
-                            # Note == "Arizona (D-201)" ~ "USA",
-                            # Note == "New Mexico (F-302)" ~ "USA",
-                            # Note == "New Mexico (F-304)" ~ "USA",
-                            # Note == "Arizona (F-306)" ~ "USA",
-                            # Note == "Arizona (D-109)" ~ "USA",
-                            # Note == "New Mexico (F-305)" ~ "USA",
-                            # Note == "Arizona (A-102)" ~ "USA",
-                            # Note == "Costa Rican adult" ~ "Costa Rica",
-                            # Note == "Costa Rican neonate (6-week-old)" ~ "Costa Rica",
-                            # Note == "Costa rica" ~ "Costa Rica",
-                            str_detect(Note, "Mexican") ~ "Mexico",
-                            # Note == "Mexican adult" ~ "Mexico",
-                            # Note == "Mexican neonate" ~ "Mexico",
-                            # Note == "Colorado (adult female)" ~ "USA",
-                            # Note == "Colorado (adult male)" ~ "USA",
-                            # Note == "Colorado (neonate female)" ~ "USA",
-                            # Note == "Colorado (neonate male)" ~ "USA",
                             Note == "Woodlark island" ~ "Papua New Guinea",
-                            # Note == "Kansas" ~ "USA",
-                            # Note == "Colorado" ~ "USA",
-                            # Note == "Ohio" ~ "USA",
-                            # Note == "Forida" ~ "USA",
                             Note == "Saibai Island" ~ "Australia",
-                            # Note == "Costa Rican Golfo de Papagayo" ~ "Costa Rica",
-                            str_detect(Note, 'India') ~ "India",
                             Note == "Java Island" ~ "Indonesia",
-                            # Note == "East India" ~ "India",
-                            # Note == "North-west India" ~ "India",
-                            # Note == "Costa Rican neonate" ~ "Costa Rica",
                             Note == "Chinese adult" ~ "China",
                             TRUE ~ Note)) %>% 
-                            # Note == "West India" ~ "India")) %>% 
   rename(Region = new_col) %>%
-  # mutate(Region = coalesce(new_col, Note)) %>%
   select(-c("Note", "Sum, %"))
 
-#%>% 
- # subset(data_clean, select=c(1,"Region",3:-1))
-   
-
-#To do: 
-# Ryk kolonnen med countries, som en af de f??rste
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-###### Clean toxin names (Kan overvejes at undlades, da det er rart at kende enheden)
+# Remove % from toxin names (Optional) ------------------------------------
 #rm_percent <- function(string){
 #  string <- string %>% 
 #    str_sub(start = 1, end = str_length(string)-3)
