531fe74ee28f1a65e313d068d93ba673fe30a5de
Sofie Olund Villumsen <sofieovillumsen@hotmail.com>
2020-05-06 07:39:13 +0000

    Adding percentage to PCA

--- R/06_model_PCA.R
+++ R/06_model_PCA.R
@@ -1,5 +1,6 @@
 library(broom)
 library(ggplot2)
+library(tidyverse)
 
 ###### Load augmented data
 data <- read_csv("data/03_data_aug.csv")  
@@ -9,20 +10,40 @@ data_new <- data %>%
   as_tibble %>%
   select_if(is.numeric)
 
+#Create PCA object
 data_pca <- data_new %>%
   prcomp(center = TRUE, scale. = TRUE)
 
+#Scree plot using broom to tidy
 data_pca %>% 
   tidy("pcs") %>% 
   ggplot(aes(x = PC, y = percent)) +
   geom_col() +
   theme_bw()
 
-data_pca_aug <- data_pca %>% augment(data)
+#Augment
+data_pca_aug <- data_pca %>% 
+  augment(data)
 
+#Adding percentage to the PCA plot
+x <- data_pca %>% 
+  tidy("pcs") %>% 
+  filter(PC==1) %>% 
+  pull(percent)
+x <- str_c("PC1 (", round(x*100, 2), "%)")
+
+y <- data_pca %>% 
+  tidy("pcs") %>% 
+  filter(PC==2) %>% 
+  pull(percent)
+y <- str_c("PC2 (", round(y*100, 2), "%)")
+
+#Plot PCA
 data_pca_aug %>% 
   ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = family)) +
-  geom_point()
+  geom_point() + 
+  labs(x = x, y = y)
+
 
 ###### K-means
 data_k_org <- data_pca_aug %>%
