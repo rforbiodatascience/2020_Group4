bb47c4b92700213441f1dfeb591e3bef5a2e4b3c
nikolasthuesen <nikolasht@hotmail.com>
2020-05-07 09:42:02 +0000

    Updated ANN

--- R/05_model_ANN.R
+++ R/05_model_ANN.R
@@ -1,14 +1,22 @@
-source('R/05_ANN_setup.R')
+#source('R/05_ANN_setup.R')
 
-###### Load augmented data
-data <- read_csv("data/03_data_aug.csv")    
+rm(list=ls())
 
-nn_dat <- data %>% as_tibble %>%
-  #Add genus labels and factors
-  mutate(class_num = as.numeric(as.factor(Genus)) - 1, # factor, so = 0, 1, 2
-         class_label = as.factor(Genus)) %>%
+library('tidyverse')
+library('keras')
+
+###### Load augmented data, and filter for low observations
+data <- read_csv("data/03_data_aug.csv")  
+
+
+##### Load data into test and training
+
+nn_dat <- data %>%
+  #Add family labels and factors
+  mutate(class_num = as.numeric(as.factor(family)) - 1, # factor, so = 0, 1
+         class_label = as.factor(family)) %>%
   #Reorganise order of columns
-  select(1:Note, class_label, class_num, everything())
+  select(1:Region, class_label, class_num, everything())
 nn_dat %>% head(3)
 
 test_f <- 0.20
@@ -19,38 +27,45 @@ nn_dat <- nn_dat %>%
                             prob = c(1 - test_f, test_f)))
 nn_dat %>% count(partition)
 
-
 x_train <- nn_dat %>%
   filter(partition == 'train') %>%
   select_if(is.numeric) %>%
-  select(class_num) %>%
-  as.matrix
+  select(-class_num) %>%
+  as.matrix()
 
-#This is where it crashes:
 y_train <- nn_dat %>%
   filter(partition == 'train') %>%
   pull(class_num) %>%
-  as.matrix
+  to_categorical
 
 x_test <- nn_dat %>%
   filter(partition == 'test') %>%
   select_if(is.numeric) %>%
-  select(class_num) %>%
+  select(-class_num) %>%
   as.matrix
 y_test <- nn_dat %>%
   filter(partition == 'test') %>%
   pull(class_num) %>%
-  as.matrix
+  to_categorical
 
+
+#### OLD MODEL:
+
+#Number of features is number of input venoms (38)
+n_features = ncol(x_train)
+
+#Number of classes is the number of snake families investigated (2)
+n_classes = length(unique(nn_dat$family))
 
 model <- keras_model_sequential() %>% 
-  layer_dense(units = 4, activation = 'relu', input_shape = 4) %>% 
-  layer_dense(units = 3, activation = 'softmax')
+  layer_dense(units = 4, activation = 'relu', input_shape = n_features) %>% 
+  layer_dense(units = 4, activation = 'relu', input_shape = 32) %>%
+  layer_dense(units = n_classes, activation = 'softmax')
 
 
 model %>%
-  compile(loss = 'categorical_crossentropy',
-          optimizer = optimizer_rmsprop(lr = 0.01),
+  compile(loss = 'binary_crossentropy',
+          optimizer = optimizer_rmsprop(lr = 0.005),
           metrics = c('accuracy')
   )
 
@@ -58,8 +73,8 @@ model %>%
 history <- model %>%
   fit(x = x_train,
       y = y_train,
-      epochs = 200,
-      batch_size = 20,
+      epochs = 150,
+      batch_size = 50,
       validation_split = 0.2
   )
 
@@ -79,8 +94,8 @@ plot_dat %>% select(-contains("feat")) %>% head(3)
 
 title     = "Classification Performance of Artificial Neural Network"
 sub_title = str_c("Accuracy = ", round(perf$acc, 3) * 100, "%")
-x_lab     = "True iris class"
-y_lab     = "Predicted iris class"
+x_lab     = "True snake family"
+y_lab     = "Predicted snake family"
 accuracy_plot <- plot_dat %>% ggplot(aes(x = class_num, y = y_pred, colour = Correct)) +
   geom_jitter() +
   scale_x_discrete(labels = levels(nn_dat$class_label)) +
