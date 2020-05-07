246d31ee3308918127aca1005ac4ba81e6b7adb9
shdam <soren.helweg@gmail.com>
2020-05-07 07:02:20 +0000

    Plots

--- R/04_analysis.R
+++ R/04_analysis.R
@@ -53,3 +53,31 @@ data_aug %>%
   arrange(desc(n)) %>% 
   View()
 
+# Bar chart comparing within snake species
+data_aug %>% 
+  filter(Snake == "Naja kaouthia") %>%
+  mutate(id = paste(Snake, " (", row_number(), ")", sep = "")) %>%
+  pivot_longer(colnames(toxins), names_to = "Toxin") %>% 
+  mutate(value = round(value, 2)) %>% 
+  rename(Value = value) %>% 
+  arrange(desc(Snake)) %>% 
+  ggplot(aes(x = id, y = Value, fill = Toxin)) +
+  geom_col() +
+  coord_flip() +
+  theme(legend.position = "none") +
+  ylab('Venom composition (%)')
+
+
+data_aug %>% 
+  filter(Snake %in% c("Naja kaouthia", "Bothrops atrox")) %>%
+  pivot_longer(colnames(toxins), names_to = "Toxin") %>% 
+  # group_by(Snake) %>%
+  # summarise(value/sum(value)) %>% View()
+  mutate(value = round(value, 2)) %>%
+  rename(Value = value) %>%
+  arrange(desc(Snake)) %>% 
+  ggplot(aes(x = Snake, y = Value, fill = Toxin)) +
+  geom_col() +
+  coord_flip() +
+  theme(legend.position = "none") +
+  ylab('Venom composition (%)')
