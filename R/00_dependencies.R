4460dabc07dab9ef2260450d8ce9b51c0fb19661
nikolasthuesen <nikolasht@hotmail.com>
2020-05-10 13:53:20 +0000

    various updates

--- R/00_dependencies.R
+++ R/00_dependencies.R
@@ -1,6 +1,6 @@
 # Setup script with all packages
 list.of.packages <- c("httr", "tidyverse", "readxl", "googlesheets4", 
-                      "shiny", "knitr", "plotly", "maps")
+                      "shiny", "knitr", "plotly", "maps", "patchwork")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if( length(new.packages) ) {
