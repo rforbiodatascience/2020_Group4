29fc27a54f732d17994fbec820603293c91dab5b
shdam <soren.helweg@gmail.com>
2020-05-10 09:52:01 +0000

    Reorder analysis

--- R/04_analysis.R
+++ R/04_analysis.R
@@ -4,7 +4,7 @@ library(maps)
 source('R/99_proj_func.R')
 
 # Store plotly files in results folder
-results_dir <- paste0(getwd(),"/results")
+results_dir <- paste0(getwd(), "/results")
 
 # Load augmented data -----------------------------------------------------
 data_aug <- read_csv("data/03_data_aug.csv")
@@ -13,63 +13,14 @@ toxin_names <- data_aug %>%
   select_if(is.numeric) %>% 
   colnames()
 
-# Distribution of genera --------------------------------------------------
-genus_count <- data_aug %>% 
-  distinct(Snake, Genus, Family) %>% 
-  ggplot(aes(y = Genus, fill = Family)) +
-  labs(x = "Count", title = "Count of distinct snakes in genera") +
-  geom_bar()
-ggsave("results/04_genus_distribution.png", plot = genus_count, device = "png")
 
-
-# Most common toxins for each genus ---------------------------------------
-# data_aug %>% 
-#   select(Snake, Genus, toxin_names) %>% 
-#   group_by(Genus) %>% 
-#   summarise_all(is_not_zero) %>% 
-#   filter(Genus %in% c("Bothrops")) %>%
-#   pivot_longer(-c(Genus, Snake)) %>% 
-#   arrange(desc(value)) %>% 
-#   ggplot(aes(x = value, y = name, fill = value)) +
-#   geom_col() + 
-#   labs(y = "Toxins", title = "Abundance of toxins in 'Bothrops'")
-
-
-# Which toxin is most abundant for each genus -----------------------------
-
-genus_num <- data_aug %>% 
-  count(Genus)
-data_aug %>% 
-  select(Genus, Family, toxin_names) %>% 
-  group_by(Genus, Family) %>% 
-  summarise_all(sum) %>% 
-  pivot_longer(-c(Genus, Family), names_to = "Toxin") %>% 
-  group_by(Family, Genus) %>%
-  filter(value == max(value)) %>% 
-  inner_join(genus_num, by = "Genus") %>% 
-  mutate(avg_abundance = round(value / n, 2)) %>% 
-  ggplot(aes(x = avg_abundance, y = Genus, fill = Toxin)) +
-  geom_col(width = 0.7) +
-  facet_grid(. ~ Family,
-             space = "free",
-             scales = "free") +
-  coord_flip()+
-  theme(axis.text.x = element_text(angle = 90,
-                                   hjust = 1,
-                                   vjust = 0.4)) +
-  labs(title = "Average abundance",
-       subtitle = "Comparing all genera",
-       x = "Average abundance (%)")
-ggsave("results/04_avg_toxin_genus.png", device = "png")
-
-# Country with most different snakes --------------------------------------
+# World map ---------------------------------------------------------------
+# Country with most different snakes
 data_world <- data_aug %>% 
   distinct(Country, Snake) %>% 
   count(Country) %>%
   arrange(desc(n))
 
-
-# World map ---------------------------------------------------------------
 map.world <- map_data("world")
 
 map.world_joined <- map.world %>% 
@@ -82,17 +33,78 @@ world <- map.world_joined %>%
              group = group,
              fill = count,
              label = region)) +
-    geom_polygon() +
-    scale_fill_gradient(low = "#ffded2",
-                        high = "red") +
-    labs(title = "World map of snake counts",
-         x = "Longitude",
-         y = "Latitude",
-         fill = "Snake count")
+  geom_polygon() +
+  scale_fill_gradient(low = "#ffded2",
+                      high = "red") +
+  labs(title = "World map of snake counts",
+       x = "Longitude",
+       y = "Latitude",
+       fill = "Snake count")
 # Too big to be stored as html
 ggsave(filename = "results/04_world_of_snakes.png", device = "png")
 
-# Bar chart comparing within snake species --------------------------------
+
+
+# Distribution of genera --------------------------------------------------
+genus_count <- data_aug %>% 
+  distinct(Snake, Genus, Family) %>% 
+  ggplot(aes(y = Genus, fill = Family)) +
+  labs(x = "Count", title = "Count of distinct snakes in genera") +
+  geom_bar()
+ggsave("results/04_genus_distribution.png", plot = genus_count, device = "png")
+
+
+# Compare venom compostion of the two snake families ----------------------
+family_toxins <- data_aug %>% 
+  pivot_longer(toxin_names,
+               names_to = "Toxin",
+               values_to = "Value") %>% 
+  group_by(Family, Toxin) %>%
+  summarise(mean(Value)) %>%
+  mutate(Value = round(`mean(Value)`, 2)) %>%
+  ggplot(aes(x = Family, y = Value, fill = Toxin)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "Mean Venom Composition of Viperidae and Elapidae",
+       y = "Mean venom composition (%)") +
+  theme(legend.position = "none")
+
+# Save plot in html file
+family_plotly <- ggplotly(family_toxins)
+htmlwidgets::saveWidget(family_plotly, file = paste0(results_dir, "/04_family_plotly.html"))
+
+
+# Which toxin is most abundant for each genus -----------------------------
+genus_num <- data_aug %>% 
+  count(Genus)
+data_aug %>% 
+  select(Genus, Family, toxin_names) %>% 
+  group_by(Genus, Family) %>% 
+  summarise_all(sum) %>% 
+  pivot_longer(-c(Genus, Family), names_to = "Toxin") %>% 
+  group_by(Family, Genus) %>%
+  filter(value == max(value)) %>% 
+  inner_join(genus_num, by = "Genus") %>% 
+  mutate(avg_abundance = round(value / n, 2)) %>%
+  ggplot(aes(x = Genus, y = avg_abundance, fill = Toxin)) +
+  geom_col(width = 0.7) +
+  facet_grid(. ~ Family,
+             space = "free",
+             scales = "free") +
+  ylim(0, 100) +
+  theme(axis.text.x = element_text(angle = 90,
+                                   hjust = 1,
+                                   vjust = 0.4)) +
+  # scale_fill_brewer(palette="Set1") +
+  # scale_colour_manual(values = c("#f8766d", "#00b7e9", "#b584ff", "#ec68ee", "#f663e1"), aesthetics = "fill") +
+  labs(title = "Average abundance",
+       subtitle = "Comparing all genera",
+       y = "Average abundance (%)")
+
+ggsave("results/04_avg_toxin_genus.png", device = "png")
+
+
+# Intra species comparizon --------------------------------
 intra_species <- data_aug %>% 
   filter(Snake == "Naja kaouthia") %>%
   mutate(Snake = paste(Snake, " (",
@@ -101,7 +113,6 @@ intra_species <- data_aug %>%
   pivot_longer(toxin_names,
                names_to = "Toxin",
                values_to = "Value") %>% 
-  # mutate(Value = round(Value, 2)) %>% 
   arrange(desc(Snake)) %>% 
   ggplot(aes(x = Snake, y = Value, fill = Toxin)) +
   geom_col() +
@@ -139,21 +150,3 @@ compareTwo <- ggplotly(compareTwo) %>%
 htmlwidgets::saveWidget(compareTwo, file = paste0(results_dir, "/04_compareTwo.html"))
 
 
-# Compare venom compostion of the two snake families ----------------------
-family_toxins <- data_aug %>% 
-  pivot_longer(toxin_names,
-               names_to = "Toxin",
-               values_to = "Value") %>% 
-  group_by(Family, Toxin) %>%
-  summarise(mean(Value)) %>%
-  mutate(Value = round(`mean(Value)`, 2)) %>%
-  ggplot(aes(x = Family, y = Value, fill = Toxin)) +
-  geom_col() +
-  coord_flip() +
-  labs(title = "Mean Venom Composition of Viperidae and Elapidae",
-       y = "Mean venom composition (%)") +
-  theme(legend.position = "none")
-
-# Save plot in html file
-family_plotly <- ggplotly(family_toxins)
-htmlwidgets::saveWidget(family_plotly, file = paste0(results_dir, "/04_family_plotly.html"))
