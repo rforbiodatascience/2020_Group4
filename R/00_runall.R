3ffe739b3ee96df88febaef17b91edd2aec5e9e3
nikolasthuesen <nikolasht@hotmail.com>
2020-05-10 11:53:21 +0000

    updated continent ANN

--- R/00_runall.R
+++ R/00_runall.R
@@ -6,6 +6,7 @@ source('R/03_augment.R')
 source('R/04_analysis.R')
 #Due to memory limitations in Rstudio Cloud, this code is not able to run here.
 #The results of the analysis are therefore obtained by running the exact same code in a new project
-#source('R/05_model_ANN.R')
+#source('R/05_ANN_family.R')
+#source('R/05_ANN_continents.R')
 source('R/06_model_PCA.R')
 # Shiny app uploaded to: https://shdam.shinyapps.io/compareTwo/
