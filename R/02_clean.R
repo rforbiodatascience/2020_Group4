40e1199064019059ee7c1a967726cbc71bab3b3c
shdam <soren.helweg@gmail.com>
2020-05-11 08:49:34 +0000

    Renaming and restructuring

--- R/02_clean.R
+++ R/02_clean.R
@@ -11,6 +11,7 @@ Brazilian_cities <- c("Juazeiro", "Ceara", "Paraiba", "Pernambuco", "ilha de Ita
 USA <- c("Colorado", "Arizona", "Idyllwild", "Loma Linda", "Phelan", "Catalina",
          "Texas", "Kentucky", "Missouri", "Florida", "Kansas", "Colorado", "Ohio",
          "New Mexico", "Forida")
+unknown <- c("Origin unknown", "neonate", "adult")
 
 data_clean <- data_raw %>% 
   rename_if(is_double, rm_percent) %>% 
@@ -22,15 +23,13 @@ data_clean <- data_raw %>%
   mutate(Country = case_when(
                             detect_in_list(Note, USA) ~ "USA",
                             Note %in% Brazilian_cities ~ "Brazil",
+                            Note %in% unknown ~ "Unknown",
                             str_detect(Note, "Caribbean") ~ "Costa Rica",
                             str_detect(Note, "Pacific") ~ "Costa Rica",
                             str_detect(str_to_lower(Note), "costa rica") ~ "Costa Rica",
                             str_detect(Note, "Venezuelan") ~ "Venezuela",
                             str_detect(Note, "Mexican") ~ "Mexico",
                             str_detect(Note, 'India') ~ "India",
-                            Note == "origin unknown" ~ "Unknown",
-                            Note == "neonate" ~ "Unknown",
-                            Note == "adult" ~ "Unknown",
                             Note == "Carribean" ~ "Costa Rica",
                             Note == "Columbia" ~ "Colombia",
                             Note == "Marocco" ~ "Morocco",
@@ -45,30 +44,7 @@ data_clean <- data_raw %>%
                             TRUE ~ Note)) %>% 
   select(-c("Note", "Sum"))
 
-# Add new data ------------------------------------------------------------
-new_data <- read_csv("data/_raw/01.2_new_data.csv")
-
-data_joined <- data_clean %>% 
-  full_join(new_data) %>%
-  mutate_each(list(~replace(., which(is.na(.)), 0)))
-
-
-# Add Continent column ----------------------------------------------------
-America <- c("Argentina", "Bolivia", "Brazil", "Colombia", "Ecuador", 
-             "Guyana", "Peru", "Venezuela", "USA", "Burkina faso", 
-             "Caribbean", "Costa Rica", "Guatemala", "Honduras", "Mexico", "Pacific")
-Eurasia <- c("France", "Russia", "Croatia", "China", "Malaysia", "Vietnam", "Taiwan", "Thailand", "Armenia", "India", "Iran", "Pakistan", "Sri Lanka", "Turkey")
-Oceania <- c("Australia", "Indonesia", "New Guinea", "Papua New Guinea")
-Africa <- c("Ghana", "Kenya", "Morocco", "Nigeria", "North Africa", "Tanzania", "Tunisia", "Uganda")
-
-data_joined <- data_joined %>% 
-  mutate(Continent = case_when(
-    Country %in% America ~ "America",
-    Country %in%  Eurasia ~ "Eurasia",
-    Country %in% Oceania ~ "Oceania",
-    Country %in%  Africa ~ "Africa",
-    TRUE ~ Country))
 
 # Write output clean file -------------------------------------------------
-data_joined %>% 
+data_clean %>% 
   write_csv('data/02_data_clean.csv')
