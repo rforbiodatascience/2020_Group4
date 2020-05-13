bb0fa97a8c2e5c5277a5c3a881028668b09e4396
shdam <soren.helweg@gmail.com>
2020-05-13 07:59:54 +0000

    dependencies

--- R/00_dependencies.R
+++ R/00_dependencies.R
@@ -2,8 +2,8 @@
 rm(list = ls())
 
 # Install packages only if not previously installed
-list_of_packages <- c("httr", "tidyverse", "readxl", "shiny", "devtools",
-                      "knitr", "plotly", "maps", "patchwork", "rsconnect", "keras")
+list_of_packages <- c("httr", "tidyverse", "readxl",
+                      "knitr", "plotly", "maps", "patchwork")
 
 new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
 if(length(new_packages)) {
